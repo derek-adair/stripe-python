6e772b06ab5528e56068800aee6b9e48daff0506
Derek Adair <d@derekadair.com>
2017-09-25 11:44:29 -0500

    Divio addon config

---
+++ aldryn_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+from aldryn_client import forms
+from aldryn_client import forms
+
+class Form(forms.BaseForm):
+    def to_settings(self, data, settings):
+        settings["PINAX_STRIPE_PUBLIC_KEY"] = os.environ.get("STRIPE_PUBLIC_KEY", "your test public key")
+        settings["PINAX_STRIPE_SECRET_KEY"] = os.environ.get("STRIPE_PRIVATE_KEY", "your test public key")
+        return settings
